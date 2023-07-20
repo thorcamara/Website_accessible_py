a385e7208f621c1d6c3a874c4432e839e99cb407
Thor <102772895+thorcamara@users.noreply.github.com>
2023-07-19 23:32:45 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import urllib.request
 
 try:
-    website = urllib.request.urlopen('https://www.youtube.com/')
+    website = urllib.request.urlopen('https://www.google.com/')
 except urllib.error.URLError:
-    print('The website Youtube is not accessible at the moment.')
+    print('The website Google is not accessible at the moment.')
 else:
-    print('I managed to access the website Youtube')
+    print('I managed to access the website Google')
